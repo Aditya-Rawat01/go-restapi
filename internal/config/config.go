6b67e06d848c974f1a14f56a6fe12a4cf1574c03
Aditya Rawat <adityarawat240@gmail.com>
2026-09-01 10:47:22 +0530

    config and loading config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,12 +1,41 @@
 package config
 
-type HTTPSERVER {
+import (
+	"flag"
+	"log"
+	"os"
+)
+
+type HTTPSERVER struct {
 	Addr string
 }
 
-type Config stuct {
-	Env string
-	StoragePath string
-	HTTPSERVER
+type Config struct {
+	Env         string `yml:"env" env:"ENV" env-required:"true" env-default:"production"`
+	StoragePath string `yml: "storage_path" env-required:"true"`
+	HTTPSERVER `yml: "http_server"`
+}
+
+func MustLoad() *Config {
+	var cfg Config
+
+	configPath := os.Getenv("CONFIG_PATH")
+
+	if configPath == "" {
+		flags := flag.String("config", "", "path to config file")
+		flag.Parse()
+
+		configPath = *flags
+
+		if configPath == "" {
+			log.Fatalf("config file doesnt exist: %s", configPath)
+		}
+	}
+
+	if _, err := os.Stat(configPath); err != nil {
+		log.Fatalf("Error while reading config %s", err.Error())
+	}
+
+	return &cfg
 
 }
